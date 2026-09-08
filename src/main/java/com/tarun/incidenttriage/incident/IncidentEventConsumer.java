726505d049606320b972b52ce47e3676b77d4940
tarunreddy112233 <tlakkireddy@uco.edu>
2026-09-08 12:27:13 -0400

    messaging: persist Kafka incident events asynchronously

---
+++ src/main/java/com/tarun/incidenttriage/incident/IncidentEventConsumer.java
@@ -0,0 +1,18 @@
+package com.tarun.incidenttriage.incident;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class IncidentEventConsumer {
+    private final IncidentService incidentService;
+
+    public IncidentEventConsumer(IncidentService incidentService) {
+        this.incidentService = incidentService;
+    }
+
+    @KafkaListener(topics = IncidentService.INCIDENT_TOPIC, groupId = "incident-persistence")
+    public void consume(IncidentEvent event) {
+        incidentService.persist(event);
+    }
+}
