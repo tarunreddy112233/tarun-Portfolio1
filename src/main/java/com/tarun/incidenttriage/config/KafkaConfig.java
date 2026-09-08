55ce1dbce3a43edf2ee7019f4d3d2ca0a09f5c2c
tarunreddy112233 <tlakkireddy@uco.edu>
2026-09-08 12:27:21 -0400

    fix: rely on Spring Boot Kafka auto-configuration

--- src/main/java/com/tarun/incidenttriage/config/KafkaConfig.java
+++ src/main/java/com/tarun/incidenttriage/config/KafkaConfig.java
@@ -1,20 +1,9 @@
 package com.tarun.incidenttriage.config;
 
-import com.tarun.incidenttriage.incident.IncidentEvent;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.support.serializer.JsonSerializer;
-import org.springframework.kafka.core.ProducerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.JacksonUtils;
-import org.springframework.kafka.support.serializer.JsonDeserializer;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-
-import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
@@ -25,28 +14,5 @@ public class KafkaConfig {
                 .partitions(3)
                 .replicas(1)
                 .build();
-    }
-
-    @Bean
-    public ProducerFactory<String, IncidentEvent> incidentProducerFactory() {
-        Map<String, Object> props = Map.of(
-                "bootstrap.servers", "${spring.kafka.bootstrap-servers:localhost:9092}"
-        );
-        return new DefaultKafkaProducerFactory<>(props);
-    }
-
-    @Bean
-    public ConsumerFactory<String, IncidentEvent> incidentConsumerFactory() {
-        Map<String, Object> props = Map.of(
-                "bootstrap.servers", "${spring.kafka.bootstrap-servers:localhost:9092}"
-        );
-        JsonDeserializer<IncidentEvent> deserializer = new JsonDeserializer<>(IncidentEvent.class, JacksonUtils.enhancedObjectMapper());
-        deserializer.addTrustedPackages("com.tarun.incidenttriage.incident");
-        return new DefaultKafkaConsumerFactory<>(props, new org.apache.kafka.common.serialization.StringDeserializer(), deserializer);
-    }
-
-    @Bean
-    public KafkaTemplate<String, IncidentEvent> incidentKafkaTemplate(ProducerFactory<String, IncidentEvent> producerFactory) {
-        return new KafkaTemplate<>(producerFactory);
     }
 }
