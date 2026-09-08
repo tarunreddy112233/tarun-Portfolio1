4a990f81ba2846f062d4310b19ed372adedb9b2d
tarunreddy112233 <tlakkireddy@uco.edu>
2026-09-08 12:28:09 -0400

    data: optimize incident id lookup

--- src/main/java/com/tarun/incidenttriage/incident/IncidentRepository.java
+++ src/main/java/com/tarun/incidenttriage/incident/IncidentRepository.java
@@ -3,9 +3,10 @@ package com.tarun.incidenttriage.incident;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface IncidentRepository extends JpaRepository<Incident, Long> {
     List<Incident> findTop50ByOrderByOccurredAtDesc();
-    boolean existsByIncidentId(UUID incidentId);
+    Optional<Incident> findByIncidentId(UUID incidentId);
 }
