19253bb13388ba2e8b4ad13f1f64c8b8934fdcbd
tarunreddy112233 <tlakkireddy@uco.edu>
2026-09-08 12:25:53 -0400

    app: add Spring Boot application entry point

---
+++ src/main/java/com/tarun/incidenttrIage/IncidentTriageApplication.java
@@ -0,0 +1,11 @@
+package com.tarun.incidenttriage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentTriageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IncidentTriageApplication.class, args);
+    }
+}
